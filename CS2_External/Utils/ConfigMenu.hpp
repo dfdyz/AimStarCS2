1e196edff47a632ce0ff48892f05e8d724282470
ORi (AimStar DEV) <152011726+Nx0Ri@users.noreply.github.com>
2023-11-26 23:51:59 +0700

    Delete CS2_External directory

--- CS2_External/Utils/ConfigMenu.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "..\Sources\Language.h"
-
-namespace ConfigMenu {
-    void RenderConfigMenu(const char *Tab);
-    void ResetToDefault();
-    // Define other configuration-related functions and variables here.
-}
